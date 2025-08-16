e46b5ca5a3dc6ce09a500d64431e8a1664824b48
Pathats <Pathat@mail.com>
2025-08-16 01:53:17 +0100

    black box

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import pygame
-
 from constants import *
+from player import Player
+
 def main():
-    color = "black"
     pygame.init()
-    print(
-        "Starting Asteroids!\n"
-        f"Screen width: {SCREEN_WIDTH}\n"
-        f"Screen height: {SCREEN_HEIGHT}"
-        )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  # Set on the class, not instance
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        screen.fill(color)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+        screen.fill("black")
         
+        for obj in drawable:
+            obj.draw(screen)
+            
+        pygame.display.flip()
 
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
